ae88e0b86ccae9da07b5a5a0e141ea1aac6128e2
king <miabiyewisdom@gmail.com>
2022-04-13 23:03:52 -0700

    Modified Html layout and css styling

--- src/App.js
+++ src/App.js
@@ -35,108 +35,90 @@ function pytorchBuild(parent, build) {
 
 function App() {
  return(
-<div className="row">
+<div className="container">
     <div className="col-md-3 headings">
-      <div className="col-md-12 title-block">
-        <div className="option-text">PyTorch Build</div>
+      <div className="">
+        <div className="">PyTorch Build</div>
       </div>
-      <div className="col-md-12 title-block">
-        <div className="option-text os-text">Your OS</div>
+      <div className="">
+        <div className="">Your OS</div>
       </div>
-      <div className="col-md-12 title-block">
-        <div className="option-text">Package</div>
+      <div className="">
+        <div className="">Package</div>
       </div>
-      <div className="col-md-12 title-block">
-        <div className="option-text">Language</div>
+      <div className="">
+        <div className="">Language</div>
       </div>
-      <div className="col-md-12 title-block">
-        <div className="option-text">Compute Platform</div>
+      <div className="">
+        <div className="">Compute Platform</div>
       </div>
-      <div className="col-md-12 title-block command-block">
-        <div className="option-text command-text">Run this Command:</div>
+      <div className="">
+        <div className=" c">Run this Command:</div>
       </div>
     </div>
 
     <div className="col-md-9">
       <div className="row ptbuild">
-        <div className="col-md-12 title-block mobile-heading">
-          <div className="option-text">PyTorch Build</div>
+        <div className="structure width-33" id="stable">
+          <div className="">Stable (1.11.0)</div>
         </div>
-        <div className="col-md-4 option block version selected" id="stable">
-          <div className="option-text">Stable (1.11.0)</div>
+        <div className="structure width-33" id="preview">
+          <div className="">Preview (Nightly)</div>
         </div>
-        <div className="col-md-4 option block version" id="preview">
-          <div className="option-text">Preview (Nightly)</div>
-        </div>
-        <div className="col-md-4 option block version" id="lts">
-          <div className="option-text">LTS (1.8.2)</div>
+        <div className="structure width-33" id="lts">
+          <div className="">LTS (1.8.2)</div>
         </div>
       </div>
       <div className="row os">
-        <div className="col-md-12 title-block mobile-heading">
-          <div className="option-text">Your OS</div>
+        <div className="structure width-33" id="linux">
+          <div className="">Linux</div>
         </div>
-        <div className="col-md-4 option block selected" id="linux">
-          <div className="option-text">Linux</div>
+        <div className="structure width-33" id="macos">
+          <div className="">Mac</div>
         </div>
-        <div className="col-md-4 option block" id="macos">
-          <div className="option-text">Mac</div>
-        </div>
-        <div className="col-md-4 option block" id="windows">
-          <div className="option-text">Windows</div>
+        <div className="structure width-33" id="windows">
+          <div className="">Windows</div>
         </div>
       </div>
       <div className="row package">
-        <div className="col-md-12 title-block mobile-heading">
-          <div className="option-text">Package</div>
+        <div className="structure width-25" id="conda">
+          <div className="">Conda</div>
         </div>
-        <div className="col-md-3 option block" id="conda">
-          <div className="option-text">Conda</div>
+        <div className="structure width-25" id="pip">
+          <div className="">Pip</div>
         </div>
-        <div className="col-md-3 option block selected" id="pip">
-          <div className="option-text">Pip</div>
+        <div className="structure width-25" id="libtorch">
+          <div className="">LibTorch</div>
         </div>
-        <div className="col-md-3 option block" id="libtorch">
-          <div className="option-text">LibTorch</div>
-        </div>
-        <div className="col-md-3 option block" id="source">
-          <div className="option-text">Source</div>
+        <div className="structure width-25" id="source">
+          <div className="">Source</div>
         </div>
       </div>
       <div className="row language">
-        <div className="col-md-12 title-block mobile-heading">
-          <div className="option-text">Language</div>
+        <div className="structure width-50"  id="python">
+          <div className="">Python</div>
         </div>
-        <div className="col-md-6 option block version selected" id="python">
-          <div className="option-text">Python</div>
-        </div>
-        <div className="col-md-6 option block" id="cplusplus">
-          <div className="option-text">C++ / Java</div>
+        <div className="structure width-50" id="cplusplus">
+          <div className="">C++ / Java</div>
         </div>
       </div>
       <div className="row cuda">
-        <div className="col-md-12 title-block mobile-heading">
-          <div className="option-text">Compute Platform</div>
+        <div className="structure width-25" id="cuda10.2">
+          <div className="">CUDA 10.2</div>
         </div>
-        <div className="col-md-3 option block version selected" id="cuda10.2">
-          <div className="option-text">CUDA 10.2</div>
+        <div className="structure width-25" id="cuda11.x">
+          <div className="">CUDA 11.3</div>
         </div>
-        <div className="col-md-3 option block version" id="cuda11.x">
-          <div className="option-text">CUDA 11.3</div>
+        <div className="structure width-25" id="rocm4.x">
+          <div className="">ROCM 4.5.2 (beta)</div>
         </div>
-        <div className="col-md-3 option block version" id="rocm4.x">
-          <div className="option-text">ROCM 4.5.2 (beta)</div>
-        </div>
-        <div className="col-md-3 option block version" id="accnone">
-          <div className="option-text">CPU</div>
+        <div className="structure width-25" id="accnone">
+          <div className="">CPU</div>
         </div>
       </div>
       <div className="row">
-        <div className="col-md-12 title-block command-mobile-heading">
-          <div className="option-text">Run this Command:</div>
-        </div>
-        <div className="command-container">
-          <div className="col-md-12" id="command"><pre>pip3 install torch torchvision torchaudio</pre></div>
+        <div className="command-container width-100">
+          <div className="structure" id="command"><pre>pip3 install torch torchvision torchaudio</pre></div>
         </div>
       </div>
     </div>
